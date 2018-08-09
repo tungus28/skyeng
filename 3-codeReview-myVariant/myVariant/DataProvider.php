3b8612d5d6ebd02fd79835d296067440e4874d1f
Kovtunenko Anton <desperadoman@rambler.ru>
2018-08-10 01:26:51 +0300

    Update DataProvider.php

--- 3-codeReview-myVariant/myVariant/DataProvider.php
+++ 3-codeReview-myVariant/myVariant/DataProvider.php
@@ -1,1 +1,57 @@
+<?php
 
+namespace AntKoff\Skyeng;
+
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+class DataProvider implements DataProviderInterface
+{
+     /**
+     * @param $host
+     * @param $user
+     * @param $password
+     */
+    public function __construct($host, $user, $password)
+    {
+        $this->host = $host;
+        $this->user = $user;
+        $this->password = $password;
+    }
+	
+	/**
+     * @inheritdoc
+     */
+	public function sendRequest()
+	{
+		//	
+	}	
+    /**
+     * @inheritdoc
+     */
+    public function getResponse(array $request) : array
+    {
+        $requestObj = $this->transformRequest($request);
+		
+        $responseObj = $this->sendRequest();
+		
+        return $this->transformResponse($responseObj);
+    }
+	
+    /**
+     * @param array $request
+     * @return RequestInterface
+     */
+    private function transformRequest(array $request) : RequestInterface
+    {
+        //transformation
+    }
+    /**
+     * @param ResponseInterface $response
+     * @return array
+     */
+    private function transformResponse(ResponseInterface $response) : array
+    {
+        //transformation
+    }
+}
